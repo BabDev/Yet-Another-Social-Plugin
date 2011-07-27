4096facd3bda92c8d3ed18f4f07de0d201744443
Michael Babker <mbabker@flbab.com>
2011-07-28 03:39:55 +0430

    Add Google options

--- tmpl/default.php
+++ tmpl/default.php
@@ -13,17 +13,23 @@ defined('_JEXEC') or die;
 
 <div class="yetanothersocial-container">
 <?php // Facebook Like button
-// @TODO: Param for count and size
 if ($displayFacebook): ?>
 	<div class="yetanothersocial-facebook active-<?php echo $count; ?>">
-		<fb:like href="<?php echo $siteURL.$itemURL; ?>" send="<?php echo $this->params->get('facebookSend', 'true'); ?>" layout="<?php echo $this->params->get('facebookLayout', 'button_count'); ?>" show_faces="<?php echo $this->params->get('facebookFaces', 'true'); ?>" action="<?php echo $this->params->get('facebookAction', 'like'); ?>" font="<?php echo $this->params->get('facebookFont', 'arial'); ?>" colorscheme="<?php echo $this->params->get('facebookColor', 'light'); ?>" />
+		<fb:like href="<?php echo $siteURL.$itemURL; ?>"
+			send="<?php echo $this->params->get('facebookSend', 'true'); ?>"
+			layout="<?php echo $this->params->get('facebookLayout', 'button_count'); ?>"
+			show_faces="<?php echo $this->params->get('facebookFaces', 'true'); ?>"
+			action="<?php echo $this->params->get('facebookAction', 'like'); ?>"
+			font="<?php echo $this->params->get('facebookFont', 'arial'); ?>"
+			colorscheme="<?php echo $this->params->get('facebookColor', 'light'); ?>" />
 	</div>
 <?php endif; ?>
 <?php // Google +1 button
-// @TODO: Param for count and size
 if ($displayGoogle): ?>
 	<div class="yetanothersocial-google active-<?php echo $count; ?>">
-		<g:plusone size="standard" count="true" href="<?php echo $siteURL.$itemURL; ?>"></g:plusone>
+		<g:plusone size="<?php echo $this->params->get('googleSize', 'standard'); ?>"
+			count="<?php echo $this->params->get('googleCount', 'true'); ?>"
+			href="<?php echo $siteURL.$itemURL; ?>"></g:plusone>
 	</div>
 <?php endif; ?>
 <?php // Twitter Share button
@@ -31,10 +37,10 @@ if ($displayGoogle): ?>
 if ($displayTwitter): ?>
 	<div class="yetanothersocial-twitter active-<?php echo $count; ?>">
 		<a href="http://twitter.com/share" class="twitter-share-button"
-		data-url="<?php echo $siteURL.$itemURL; ?>"
-		data-count="horizontal"
-		data-via="mbabker"
-		data-text="Check this out -">Tweet</a>
+			data-url="<?php echo $siteURL.$itemURL; ?>"
+			data-count="horizontal"
+			data-via="mbabker"
+			data-text="Check this out -">Tweet</a>
 	</div>
 <?php endif; ?>
 </div>
